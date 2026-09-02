38225501be3a07d39abb65d732152d00c057f4c9
AKHIL A <akkeduc01@gmail.com>
2026-09-02 15:07:30 +0530

    Update ak36.py

--- ak36.py
+++ ak36.py
@@ -1,3 +1,4 @@
+#simple program to understand how list sort and sorted works
 s = [10 , 20 , 30 , 40 , 50 ]
 s.sort
 print("sorted:",s)
@@ -5,3 +6,4 @@ pased = [s for s in s if s >=8]
 print("passes")
 grace_marks = [s+5 for s in s]
 print("addition od 5 grace marks",grace_marks)
+#end of the program
