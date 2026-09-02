1e83deafa37e555e434010abd3df4922f38e4064
AKHIL A <akkeduc01@gmail.com>
2026-09-02 15:03:58 +0530

    Update ak37.py

--- ak37.py
+++ ak37.py
@@ -1,5 +1,7 @@
+#simple cart filter also know as the GST + filter ing the product above 100
 p = [250 , 999 , 45 , 1500 , 60]
 a_100 = [p for p in p if p > 100]
 print("items above 100rs:",a_100)
 with_gst  = [round(p*1.18,2) for p in p]
 print("items with GST:",with_gst)
+#end of the python code
