0eb17e507a47ce3337b4f330dfc44f626e0c6308
AKHIL A <akkeduc01@gmail.com>
2026-09-04 07:33:58 +0530

    Update 4k26.py

--- 4k26.py
+++ 4k26.py
@@ -1,3 +1,4 @@
+#this program is about o find the even number in the given list
 def is_even (number):
     return number % 2 == 0
 number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
@@ -6,4 +7,5 @@ for num in number:
         print(f"{num} is even")
     else:
         print(f"{num} is odd")
-        
+
+#end of the program
