f5fbce333575bdf83c4c3a9d3fdd2b913ef8b2eb
AKHIL A <akkeduc01@gmail.com>
2026-09-04 07:21:34 +0530

    Update 4k22.py

--- 4k22.py
+++ 4k22.py
@@ -1,3 +1,6 @@
+#the program is about calucalating the simple intrest#
+# based on principal , rate and time 
+
 def calucalate_simple_interest(Principal, Rate, Time):
     simple_interest = (Principal * Rate * Time) / 100
     return simple_interest
@@ -5,3 +8,4 @@ intrest1=calucalate_simple_interest(10000, 5, 2)
 intrest2=calucalate_simple_interest(20000, 7, 3)
 print("The simple interest for the first case is:", intrest1)
 print("The simple interest for the second case is:", intrest2)
+#the end of the program 
