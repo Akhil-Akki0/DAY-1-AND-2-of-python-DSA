53d1e876a73e1e283b88c64e72d6e4800c836432
AKHIL A <akkeduc01@gmail.com>
2026-09-04 07:20:13 +0530

    Add comments to calculate_area function
    Added comments to clarify the purpose of the program and its functions.

--- 4k20.py
+++ 4k20.py
@@ -1,5 +1,8 @@
+#the program is about calucalating the area based on length and width
+
 def calculate_area(length, width):
     length = float(input("Enter the length: "))
     width = float(input("Enter the width: "))
     area = length * width
     return area
+    #end of the calucalating area program 
